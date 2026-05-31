afb6afba735274f83bb92f50ef7e7d282c690ced
chele-1024 <jayanataa24@gmail.com>
2026-05-31 22:26:54 +0700

    commit

--- home.php
+++ home.php
@@ -5,6 +5,20 @@
         header("location:index.php?p=Silahkan Login Terlebih Dahulu");
         exit();
     }
+
+    date_default_timezone_set('Asia/Jakarta');
+    $tanggal = date ("d-m-Y");
+    $jam = date ("H");
+
+    if ($jam >= 5 && $jam < 12) {
+        $salam = "Selamat Pagi";
+    } elseif ($jam >= 12 && $jam < 15) {
+        $salam = "Selamat Siang";
+    } elseif ($jam >= 15 && $jam < 18) {
+        $salam = "Selamat Sore";
+    } else {
+        $salam = "Selamat Malam";
+    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -19,6 +33,8 @@
             <div class="container">
                 <h2>APLIKASI MANAJEMEN DATA SISWA</h2>
                 <hr>
+                <p>Hari ini tanggal : <?php echo $tanggal; ?></p>
+                <p><?php echo $salam; ?>, <?php echo $_SESSION['user']; ?>!</p>
                 <p>Selamat datang di aplikasi Data Siswa SMKS PGTI 3 Malang</p>
             </div>
         </div>
--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
     $host = "localhost";
     $username = "root";
     $password = "";
-    $database = "michelle_penilaian";
+    $database = "michellereynaldi_penilaian";
 
     $koneksi = mysqli_connect($host, $username, $password, $database);
 
