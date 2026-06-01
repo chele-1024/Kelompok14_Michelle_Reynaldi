d2650acb1dee9688a3b92d323a8f9714967e4788
chele-1024 <jayanataa24@gmail.com>
2026-06-01 22:53:31 +0700

    commit

--- prodi.php
+++ prodi.php
@@ -25,7 +25,23 @@
             <a href="tambah_prodi.php" class="tambah">Tambah Data Prodi</a>
             <br><br>
             <table>
-                
+                <tr>
+                    <th>Kode Prodi</th>
+                    <th>Nama Prodi</th>
+                    <th>Action</th>
+                </tr>
+                <?php while ($row = mysqli_fetch_assoc($data)) { ?>
+                    <tr>
+                        <td><?php echo $row['kd_prodi']; ?></td>
+                        <td><?php echo $row['nama_prodi']; ?></td>
+                        <td>
+                            <a href="edit_prodi.php?id_prodi=<?php echo $row['id_prodi']; ?>">Edit</a>
+                            <a href="hapus_prodi.php?id_prodi=<?php echo $row['id_prodi']; ?>" 
+                            onclick="return confirm('Yakin ingin hapus?')">DELETE</a>
+                        </td>
+                    </tr>
+                    <?php } ?>
+            </table>
         </div>
     </div>
 </body>
