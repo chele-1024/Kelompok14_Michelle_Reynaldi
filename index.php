0353ca25eb61c6c07cc41462f3a3c17a4a280a74
chele-1024 <jayanataa24@gmail.com>
2026-06-04 06:06:13 +0700

    commit

--- index.php
+++ index.php
@@ -9,17 +9,19 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="container">
+    <div class="login-container">
         <h1>PANEL LOGIN</h1>
         <hr>
         <form action="cek_login.php" method="POST">
-            <div class="form-control">
+            <div class="form-group">
+                <label>Username</label>
                 <input type="text" name="user" placeholder="Masukkan Username" >
             </div>
-            <div class="form-control">
+            <div class="form-group">
+                <label>Password</label>
                 <input type="password" name="pass" placeholder="Masukkan Password" >
             </div>
-            <div class="form-control">
+            <div class="form-group">
                 <button type="submit">Login</button>
             </div>
         </form>
